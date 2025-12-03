eeba2d555a87938a7fc8cbbd3d9956a9f2d6838f
Audrey Luiz <audreyluiz.ls@gmail.com>
2025-12-02 23:49:08 -0300

    criando arquivos de estilo e funcoes iniciais das views

--- clientes/models.py
+++ clientes/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Cliente(models.Model):
+    codigo = models.CharField(max_length=20, unique=True, blank=True, null=True)
+    nome = models.CharField(max_length=200)
+    cpf = models.CharField(max_length=20, blank=True, null=True)
+    rg = models.CharField(max_length=20, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    telefone = models.CharField(max_length=30, blank=True, null=True)
+    whatsapp = models.CharField(max_length=30, blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        # SE O CÓDIGO NÃO EXISTIR, GERA AUTOMATICAMENTE
+        if not self.codigo:
+            last_codigo = Cliente.objects.filter(codigo__startswith='CLI').order_by('-codigo').first()
+
+            if last_codigo:
+                # extrai a parte numérica para incrementar
+                num = int(last_codigo.codigo.replace('CLI', ''))
+                self.codigo = f"CLI{num + 1:04d}"  # CLI0001, CLI0002…
+            else:
+                self.codigo = "CLI0001"
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.codigo} - {self.nome}"
--- clientes/views.py
+++ clientes/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
+
+from clientes.models import Cliente
+
 
 # Create your views here.
+def lista(request):
+    clientes = Cliente.objects.all()
+
+    context = {
+        "clientes": clientes,
+    }
+    template = loader.get_template("clientes.html")
+    return HttpResponse(template.render(context, request))
